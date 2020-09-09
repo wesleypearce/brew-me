fd2062453d082271f37154461bb6fd8bdbded9da
Wes Pearce <wespearce1@gmail.com>
2020-09-09 06:51:33 -0600

    Initial commit

---
+++ src/components/Breweries.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Brewery from './Brewery'
+
+const Breweries = () => {
+  return (
+    <div>
+      <Brewery />
+    </div>
+  )
+}
+
+export default Breweries
---
+++ src/components/Brewery.js
@@ -0,0 +1,29 @@
+import React from 'react'
+
+let brewery = {
+  id: 299,
+  name: "Almanac Beer Company",
+  brewery_type: "micro",
+  street: "651B W Tower Ave",
+  city: "Alameda",
+  state: "California",
+  postal_code: "94501-5047",
+  country: "United States",
+  longitude: "-122.306283180899",
+  latitude: "37.7834497667258",
+  phone: "4159326531",
+  website_url: "http://almanacbeer.com",
+  updated_at: "2018-08-23T23:24:11.758Z"
+}
+
+const Brewery = () => {
+
+  return (
+    <div>
+      <h2>{brewery.name}</h2>
+      <p>{brewery.city}, {brewery.state}</p>
+    </div>
+  )
+}
+
+export default Brewery
